d6331b879037c63338334565e4ba73e8f2f3ca53
Juan Carlos Vargas <Inicio@MacBook-Pro-de-Juan.local>
2020-06-03 02:47:57 +0200

    Updates

---
+++ precache-manifest.1c11dbcf74d31da3b503ba8f91ec919f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c9c9f6ffdddd1233e396eb5bea9679",
+    "url": "/southsound-dev/jamming/index.html"
+  },
+  {
+    "revision": "12083b31e4869ff9ddd6",
+    "url": "/southsound-dev/jamming/static/css/main.e1f1a08b.chunk.css"
+  },
+  {
+    "revision": "5c9db2e99d88d8f8f03e",
+    "url": "/southsound-dev/jamming/static/js/2.5d4eb589.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/southsound-dev/jamming/static/js/2.5d4eb589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12083b31e4869ff9ddd6",
+    "url": "/southsound-dev/jamming/static/js/main.27336426.chunk.js"
+  },
+  {
+    "revision": "05ed1773970ef1750e28",
+    "url": "/southsound-dev/jamming/static/js/runtime-main.3693fadd.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/southsound-dev/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
